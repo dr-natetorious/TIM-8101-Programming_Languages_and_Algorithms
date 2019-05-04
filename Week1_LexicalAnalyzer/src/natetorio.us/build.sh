8e70b8a7bf2cbd2a6711bbfba3c34cdfb224e95e
Nate Bachmeier <nate.bachmeier@renaissance.com>
2019-05-03 19:53:26 -0700

    Getting more fancy with the grammar script

---
+++ Week1_LexicalAnalyzer/src/natetorio.us/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+OUTPUT_DIRECTORY=/src/out/
+
+function build_jj(){
+  name=$1
+  java javacc -OUTPUT_DIRECTORY=${OUTPUT_DIRECTORY}/${name} "${name}.jj"
+  javac ${OUTPUT_DIRECTORY}/${name}/*.java
+  jar cvf ${name}.jar ${OUTPUT_DIRECTORY}/${name}/*.class
+}
+
+build_jj HelloWorld
+build_jj MoreStatements
