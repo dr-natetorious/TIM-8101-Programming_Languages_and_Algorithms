5046849033c4de01e198cf83e2a8d3faf58ab59b
Nate Bachmeier <nate.bachmeier@renaissance.com>
2019-05-03 22:42:40 -0700

    hello world parses with the lexical driver

--- Week1_LexicalAnalyzer/src/natetorio.us/build.sh
+++ Week1_LexicalAnalyzer/src/natetorio.us/build.sh
@@ -6,9 +6,37 @@ function build_jj(){
   name=$1
   java javacc -OUTPUT_DIRECTORY=${OUTPUT_DIRECTORY}/${name} "${name}.jj"
   javac ${OUTPUT_DIRECTORY}/${name}/*.java
-  jar cvf ${name}.jar ${OUTPUT_DIRECTORY}/${name}/*.class
+  jar cvf ${OUTPUT_DIRECTORY}/${name}.jar ${OUTPUT_DIRECTORY}/${name}/*.class
 }
 
-build_jj HelloWorld
-build_jj MoreStatements
+function header(){
+  echo "====================="
+  echo "$*"
+  echo "====================="
+  echo
+}
+
+function test_file(){
+  filename=$1
+  header $filename
+  
+  echo
+  cat $filename
+  echo
+
+  java NCU_PL_Lexer_Driver $filename
+}
+
+header "Build the .jj files"
+#build_jj HelloWorld
+#build_jj MoreStatements
 build_jj NCU_PL
+
+header "Bulid the Lexer Driver"
+javac -d ${OUTPUT_DIRECTORY}/NCU_PL -cp ../out/NCU_PL/  NCU_PL_Lexer_Driver.java
+
+header "Unit test scripts"
+pushd ${OUTPUT_DIRECTORY}/NCU_PL 
+pwd
+test_file ../../LDK/tests-programs/hello-world.ncupl
+popd
