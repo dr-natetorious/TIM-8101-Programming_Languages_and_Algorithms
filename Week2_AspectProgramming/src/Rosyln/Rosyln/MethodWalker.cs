d3a9e4d7a173c433fb7332c6f4b884d158078fd4
Nate Bachmeier <nate.bachmeier@renaissance.com>
2019-05-07 01:47:14 -0700

    Add Rosyln Tool

---
+++ Week2_AspectProgramming/src/Rosyln/Rosyln/MethodWalker.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rosyln
+{
+    public class MethodWalker : CSharpSyntaxRewriter
+    {
+        private readonly ICompilationUnitSyntax root;
+        private readonly SemanticModel model;
+
+        public MethodWalker(ICompilationUnitSyntax root, SemanticModel model)
+        {
+            this.root = root;
+            this.model = model;
+        }
+
+        public override SyntaxNode VisitMethodDeclaration(MethodDeclarationSyntax node)
+        {
+            if (node.Modifiers.Any(m => m.Text == "public") == false)
+            {
+                return node;
+            }
+
+            var checkedParameters = new List<string>();
+            var blockSyntax = node.ChildNodes().OfType<BlockSyntax>().FirstOrDefault();
+            if (blockSyntax == null)
+            {
+                return node;
+            }
+
+            // Find all the parameter checks..
+            checkedParameters.AddRange(WalkMethodBody(blockSyntax));
+
+            // Find which parameters are not checked...
+            var parameters = node.ParameterList.Parameters.Select(p => p.Identifier.Text);
+            var statements = new List<StatementSyntax>();
+            foreach (var parameter in parameters)
+            {
+                if (checkedParameters.Contains(parameter))
+                {
+                    continue;
+                }
+
+                var check = SyntaxFactory.ParseStatement(
+                    $@"
+                        // Check parameter...
+                        if({parameter} == null){{
+                            throw new System.ArgumentNullException(""{parameter}"");
+                        }}
+
+                    ");
+
+                statements.Add(check);
+            }
+
+            // If any checks are needed inject them...
+            if (statements.Count == 0)
+            {
+                return node;
+            }
+
+            statements.AddRange(blockSyntax.Statements);
+            var block = SyntaxFactory.Block(statements);
+
+            var result = node.ReplaceNode(blockSyntax, block);
+            return result;
+        }
+
+        private IReadOnlyList<string> WalkMethodBody(BlockSyntax block)
+        {
+            var checkedParameters = new List<string>();
+            foreach (var ifStatement in block.ChildNodes().OfType<IfStatementSyntax>())
+            {
+                foreach(var binaryExpression in ifStatement.ChildNodes().OfType<BinaryExpressionSyntax>())
+                {
+                    if (binaryExpression.Left is IdentifierNameSyntax identifier)
+                    {
+                        checkedParameters.Add(identifier.Identifier.Text);
+                    }
+                }
+            }
+
+            return checkedParameters;
+        }
+    }
+}
---
+++ Week2_AspectProgramming/src/Rosyln/Rosyln/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+namespace Rosyln
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var directory = new DirectoryInfo(args[0]);
+            foreach (var file in directory.GetFiles("*.cs", SearchOption.AllDirectories))
+            {
+                Console.WriteLine($"{file.FullName}...");
+                var tree = CSharpSyntaxTree.ParseText(File.ReadAllText(file.FullName));
+                var root = tree.GetCompilationUnitRoot();
+
+                var compilation = CSharpCompilation.Create("Mastery")
+                    .AddSyntaxTrees(tree);
+
+                var model = compilation.GetSemanticModel(tree);
+
+                var texas = new MethodWalker(root, model);
+                var rewrite = texas.Visit(root);
+
+                using (var sw = new StreamWriter(file.OpenWrite()))
+                {
+                    rewrite.WriteTo(sw);
+                }
+            }
+        }
+    }
+}
