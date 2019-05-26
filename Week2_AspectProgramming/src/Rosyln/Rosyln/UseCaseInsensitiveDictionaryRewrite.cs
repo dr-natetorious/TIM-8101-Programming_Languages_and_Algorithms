cc697ac99c6dd595e5de27683d57ddfedf86e39d
Nate Bachmeier <nate.bachmeier@renaissance.com>
2019-05-26 13:35:08 -0700

    week 4_bigoh

---
+++ Week2_AspectProgramming/src/Rosyln/Rosyln/UseCaseInsensitiveDictionaryRewrite.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Linq;
+
+namespace Rosyln
+{
+    class UseCaseInsensitiveDictionaryRewrite : CSharpSyntaxRewriter
+    {
+        public override SyntaxNode VisitObjectCreationExpression(ObjectCreationExpressionSyntax node)
+        {
+            if (node.Type is GenericNameSyntax typeName)
+            {
+                // Check if this is a Dictionary<T,K>
+                if (string.Equals(typeName.Identifier.Text, "Dictionary", StringComparison.Ordinal) == false)
+                {
+                    Console.WriteLine($"Skipping {typeName}...");
+                    return node;
+                }
+
+                // Check if this is a Dictionary<string, K>
+                var keyType = typeName.TypeArgumentList.Arguments[0];
+                if (keyType is PredefinedTypeSyntax predefinedType)
+                {
+                    if (string.Equals(predefinedType.Keyword.Text, "string", StringComparison.OrdinalIgnoreCase) == false)
+                    {
+                        Console.WriteLine($"Skipping {typeName}...");
+                        return node;
+                    }
+                }
+
+                // Confirm the first argument is OrdinalIgnoreCase
+                var found = false;
+                foreach(var argument in node.ArgumentList.Arguments)
+                {
+                    if (argument.Expression is MemberAccessExpressionSyntax expression)
+                    {
+                        var desired = "StringComparer.OrdinalIgnoreCase";
+                        if (string.Equals(Convert.ToString(expression.GetText()), desired))
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (found == false)
+                {
+                    node.AddArgumentListArguments(
+                        kines: SyntaxFactory.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression,
+                        SyntaxFactory.ParseExpression("System.StringComparer.OrdinalIgnoreCase"));
+                }
+            }
+
+            return node;
+        }
+    }
+}
---
+++ Week4_BigOh/Assignment.md
@@ -0,0 +1,21 @@
+# Instructions
+
+Locate a recent scholarly article (no older than 5 years) that presents an alternative to Big-O for representing algorithmic complexity.  Critique the alternative and include the following:
+
+1. An overview of Big-O, its strengths, and its weaknesses
+
+2. Your assessment of Big-O for its intended purpose
+
+3. An overview of the Big-O alternative, how it works, and how it measures algorithmic complexity
+
+4. Its strengths compared to Big-O
+
+5. Its weaknesses compared to Big-O
+
+6. Its potential to overtake Big-O as the universally accepted method for noting algorithmic complexity
+
+Length: 5 – 7 pages (not including title and references)
+
+Support your paper with at least 4 scholarly sources.
+
+Your paper should demonstrate thoughtful consideration of the ideas and concepts that are presented in the course and provide new thoughts and insights relating directly to this topic. Your response should reflect scholarly writing and current APA standards. Be sure to adhere to Northcentral University's Academic Integrity Policy.
