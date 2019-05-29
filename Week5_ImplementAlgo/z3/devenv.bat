9144910a605b6b536b0e9e525082c450393ca924
Nate Bachmeier <nate.bachmeier@renaissance.com>
2019-05-28 22:40:17 -0700

    Add docker images for z3 algos

---
+++ Week5_ImplementAlgo/z3/devenv.bat
@@ -0,0 +1,22 @@
+@ECHO ON
+@ECHO =================================
+@ECHO (c) Nate Bachmeier
+@ECHO https://github.com/dr-natetorious
+@ECHO =================================
+@SETLOCAL enableextensions enabledelayedexpansion
+@SET base_path=%~dp0
+@PUSHD %base_path%
+
+@SET ImageName=ncu/z3-dev
+
+@CALL docker build -t %ImageName% .
+@IF NOT ERRORLEVEL 0 GOTO ERROR
+@CALL docker run -it -v %base_path%\src:/src -v %userprofile%\.aws:/root/.aws -w /git/ %ImageName% bash
+@IF NOT ERRORLEVEL 0 GOTO ERROR
+GOTO END
+
+:ERROR
+@ECHO Failed to perform the last step, exiting early.
+
+:END
+@POPD
